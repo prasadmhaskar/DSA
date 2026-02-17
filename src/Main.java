e545d4dd05685d04b082285db0ac40dc63e66579
Prasad Mhaskar <prasadmhaskar33@gmail.com>
2026-02-17 16:23:08 +0530

    first push to github

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter first String:");
+//        String str1 = sc.nextLine();
+//        System.out.println("Enter second String:");
+//        String str2 = sc.nextLine();
+//
+//        System.out.println(stringMania(str1.length(), str2.length(), str1, str2));
+//    }
+//
+//    public static int stringMania(int n, int m, String str1, String str2) {
+//        // Find the length of the shorter string to avoid StringIndexOutOfBoundsException
+//        int minLength = Math.min(n, m);
+//
+//        for (int i = 0; i < minLength; i++) {
+//            char c1 = str1.charAt(i);
+//            char c2 = str2.charAt(i);
+//
+//            if (c1 > c2) return 1;
+//            if (c1 < c2) return -1;
+//        }
+//
+//        // If all characters up to minLength are equal, the longer string is "greater"
+//        if (n > m) return 1;
+//        if (m > n) return -1;
+//
+//        return 0;
 
 
-        System.out.println('c' - 'a');
+
     }
 }
